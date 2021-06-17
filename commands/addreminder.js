cd79536f800d9311385d3dc98279b42a210fdd32
udop1 <52384714+udop1@users.noreply.github.com>
2021-06-17 17:15:01 +0100

    Update addreminder.js

--- commands/addreminder.js
+++ commands/addreminder.js
@@ -1,165 +1,156 @@
-const { prefix } = require('../config.json');
+const {prefix} = require('../config.json');
 var startTime = Math.trunc(new Date().getTime() / 1000);
 
 function conversionvalue(key) {
 	if (key == "s") {
-	  return 1
+		return 1;
+	} else if (key == "m") {
+		return 60;
+	} else if (key == "h") {
+		return 3600;
+	} else if (key == "d") {
+		return 86400;
+	} else if (key == "w") {
+		return 604800;
+	} else {
+		return 0;
 	}
-	else if (key == "m") {
-	  return 60
-	}
-	else if (key == "h") {
-	  return 3600
-	}
-	else if (key == "d") {
-	  return 86400
-	}
-	else if (key == "w") {
-	  return 604800
-	}
-	else {
-	  return 0
-	}
-  }
+}
   
   
-  function absoluteresolution(timedata) {
+function absoluteresolution(timedata) {
 	let currentDate = new Date();
 	let colon = 0;
-	let slashcount = 0
+	let slashcount = 0;
   
   
 	for (let i = 0; i < timedata.length; i++) {
-	  if (timedata[i] == "/") {
-		slashcount = slashcount + 1
-	  }
+		if (timedata[i] == "/") {
+			slashcount = slashcount + 1;
+		}
 	}
   
   
 	for (let i = 0; i < timedata.length; i++) {
-	  if (timedata[i] == ":") {
-		colon = i
-	  }
+		if (timedata[i] == ":") {
+			colon = i;
+		}
 	}
-	var hour = timedata.slice(colon - 2, colon).join("")
-	var minute = timedata.slice(colon + 1, colon + 3).join("")
+	var hour = timedata.slice(colon - 2, colon).join("");
+	var minute = timedata.slice(colon + 1, colon + 3).join("");
   
   
 	if (slashcount == 0) {
-	  let cDay = currentDate.getDate()
-	  let cMonth = String(currentDate.getMonth() + 1)
-	  if(cMonth.length == 1){
-		cMonth = "0" + cMonth
-	  }
-	  let cYear = currentDate.getFullYear()
-	  let totalstring = (cYear + "-" + cMonth + "-" + cDay + "T" + hour + ":" + minute + ":00")
-	  return Date.parse(totalstring)/1000
-	}
-	let firstslash = 0
-  
-  
-	if (slashcount == 1) {
-	  var cYear = currentDate.getFullYear()
-	}
-  
-	else {
-	  for (let i = 0; i < timedata.length; i++) {
-		if (timedata[i] == "/") {
-		  firstslash = i
+		let cDay = currentDate.getDate();
+		let cMonth = String(currentDate.getMonth() + 1);
+		if(cMonth.length == 1){
+			cMonth = "0" + cMonth;
 		}
-	  }
-	  var y = (timedata.slice(firstslash+1))
-	  if(y.length == 4){
-		var year = y.join("")
-	  }
-	  else{
-		let x = String(timedata.slice(firstslash + 1, firstslash + 3).join(""))
-		var year = ("20" + x)
-	  }
+		let cYear = currentDate.getFullYear();
+		let totalstring = (cYear + "-" + cMonth + "-" + cDay + "T" + hour + ":" + minute + ":00");
+		return Date.parse(totalstring) / 1000;
 	}
-  
-  
-	for (let i = 0; i < timedata.length; i++) {
-	  if (timedata[i] == "/") {
-		firstslash = i
-		break
-	  }
-	}
-  
-	var day = (timedata.slice(firstslash - 2, firstslash)).join("")
-	if(day[0] == ' '){
-	  day = "0" + day[1]
-	}
-	var month = (timedata.slice(firstslash + 1, firstslash + 3)).join("")
-  
-	if(month[1] == '/'){
-	  month = "0" + month[0]
-	}
-	let totalstring = (year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":00")
-	return (Date.parse(totalstring)/1000)
-  }
-  
-  function relativeresolution(timedata) {
-	var startTime = Math.trunc(new Date().getTime() / 1000);
-	for (let i = 0; i < timedata.length; i++) {
-	  if (timedata[i] == " ") {
-		delete timedata[i]
-	  }
-	}
-  
-	const dataloc = new Map()
-	for (let i = 0; i < timedata.length; i++) {
-	  if (timedata[i] == "s" || timedata[i] == "m" || timedata[i] == "h" || timedata[i] == "d" || timedata[i] == "w") {
-		dataloc.set(timedata[i], i)
-	  }
-	}
-	var unixdata = 0
-	var previousval = 0
-	var i = 0
-	for (let entry of dataloc.entries()) {
-	  if (i == 0) {
-		let first_val = entry[1]
-		let convval = Number(conversionvalue(entry[0]))
-		let value = Number((timedata.slice(0, first_val)).join(""))
-		unixdata = unixdata + (convval * value)
-		previousval = first_val
-	  }
-	  else {
-		let val = entry[1]
-		let convval = Number(conversionvalue(entry[0]))
-		let value = Number((timedata.slice(previousval + 1, val).join("")))
-		previousval = val
-  
-		unixdata = unixdata + (convval * value)
-	  }
-	  i = i + 1
-	}
-	return ((unixdata + startTime))
-  }
-  
-function main(message) {
-	var inputstring = (message.split('')).slice(12)
-	for (let i = 0; i < inputstring.length; i++) {
-	  	var x = inputstring.length - i - 1 // Checks list in reverse order, so @s in the message arent counted
-	  	if (inputstring[x] == "@") {
-			var splitpoint = x
+	let firstslash = 0;
+
+	if (slashcount == 1) {
+	  	var cYear = currentDate.getFullYear();
+	} else {
+	  	for (let i = 0; i < timedata.length; i++) {
+			if (timedata[i] == "/") {
+		  		firstslash = i;
+			}
+	  	}
+	  	var y = (timedata.slice(firstslash+1));
+	  	//console.log(y);
+	  	if(y.length >= 4){
+			//console.log(y);
+			var year = (y.slice(0,4)).join("");
+	  	} else{
+			let x = String(timedata.slice(firstslash + 1, firstslash + 3).join(""));
+			var year = ("20" + x);
 	  	}
 	}
-	var timedata = inputstring.slice(0, splitpoint)
-	var content = inputstring.slice(splitpoint + 1).join("")
-	var absolute = false
+
+	for (let i = 0; i < timedata.length; i++) {
+	  	if (timedata[i] == "/") {
+			firstslash = i;
+			break;
+	  	}
+	}
+  
+	var day = (timedata.slice(firstslash - 2, firstslash)).join("");
+	if(day[0] == ' '){
+	  	day = "0" + day[1];
+	}
+	var month = (timedata.slice(firstslash + 1, firstslash + 3)).join("");
+  
+	if(month[1] == '/'){
+	  	month = "0" + month[0];
+	}
+	//console.log(year);
+	let totalstring = (year + "-" + month + "-" + day + "T" + hour + ":" + minute + ":00");
+	return (Date.parse(totalstring)/1000);
+}
+  
+function relativeresolution(timedata) {
+	var startTime = Math.trunc(new Date().getTime() / 1000);
+	for (let i = 0; i < timedata.length; i++) {
+	  	if (timedata[i] == " ") {
+			delete timedata[i];
+	  	}
+	}
+  
+	const dataloc = new Map();
+	for (let i = 0; i < timedata.length; i++) {
+	  	if (timedata[i] == "s" || timedata[i] == "m" || timedata[i] == "h" || timedata[i] == "d" || timedata[i] == "w") {
+			dataloc.set(timedata[i], i);
+	  	}
+	}
+	var unixdata = 0;
+	var previousval = 0;
+	var i = 0;
+	for (let entry of dataloc.entries()) {
+	  	if (i == 0) {
+			let first_val = entry[1];
+			let convval = Number(conversionvalue(entry[0]));
+			let value = Number((timedata.slice(0, first_val)).join(""));
+			unixdata = unixdata + (convval * value);
+			previousval = first_val;
+	  	} else {
+			let val = entry[1];
+			let convval = Number(conversionvalue(entry[0]));
+			let value = Number((timedata.slice(previousval + 1, val).join("")));
+			previousval = val;
+	
+			unixdata = unixdata + (convval * value);
+	  	}
+	  	i = i + 1;
+	}
+	return ((unixdata + startTime));
+}
+  
+function main(message) {
+	var inputstring = (message.split('')).slice(12);
+	for (let i = 0; i < inputstring.length; i++) {
+	  	var x = inputstring.length - i - 1; // Checks list in reverse order, so @s in the message arent counted
+	  	if (inputstring[x] == "@") {
+			var splitpoint = x;
+	  	}
+	}
+	var timedata = inputstring.slice(0, splitpoint);
+	var content = inputstring.slice(splitpoint + 1).join("");
+	var absolute = false;
 	for (let i = 0; i < timedata.length; i++) {
 		if (timedata[i] == ":") {
-			absolute = true
+			absolute = true;
 		}
 	}
 	if (absolute == true) {
-	  	var time = absoluteresolution(timedata)
+	  	var time = absoluteresolution(timedata);
+	} else {
+	  	var time = relativeresolution(timedata);
 	}
-	else {
-	  	var time = relativeresolution(timedata)
-	}
-	return([time,content])
+	return([time,content]);
 }
 //console.log(x[0]);
 //console.log(x[1]);
@@ -177,9 +168,9 @@ module.exports = {
         var channelIn = message.channel.id;
 
 		const inputstring = message.content;
-        x = main(inputstring)
-        const endTime = x[0]
-        const reminder = x[1]
+        x = main(inputstring);
+        const endTime = x[0];
+        const reminder = x[1];
 
         try {
             await Reminders.create({
@@ -202,6 +193,7 @@ module.exports = {
 
             //message.reply(message.url);
             return message.reply('Your reminder for ' + endDate + ' ' + endMonth + ' ' + endYear + ' at ' + endHour + ':' + endMin + ':' + endSec + ' has been set!');
+			//return message.reply(endTime);
         } catch (error) {
             console.log(error);
             message.reply('Error:\n`' + error + '`');
