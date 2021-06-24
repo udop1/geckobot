a2fe9168b5f4f46a87cade1defa4c5b92b9a79d2
udop1 <52384714+udop1@users.noreply.github.com>
2021-06-24 20:40:37 +0100

    Update deletereminder.js

--- commands/deletereminder.js
+++ commands/deletereminder.js
@@ -5,14 +5,22 @@ module.exports = {
     args: true,
     usage: '<reminder-id>',
     async execute(message, args) {
-        const {Reminders} = require('../index');
+        const {Reminders, mysql} = require('../index');
         const messageOwner = message.author.id;
         
-        const rowCount = await Reminders.destroy({where: {reminder_id: args[0], username: messageOwner}});
+        //const rowCount = await Reminders.destroy({where: {reminder_id: args[0], username: messageOwner}});
+        mysql.query("DELETE FROM tbl_Reminders WHERE reminder_id = " + mysql.escape(args[0]), function (error, result) {
+            //if (error) throw error;
+            if (error) {
+                console.log(error);
+                return message.reply('You don\'t have a reminder with that ID.');
+            }
+            console.log("Reminder ID Deleted: " + args[0]);
+        });
 
-        if (!rowCount) {
+        /*if (!rowCount) {
             return message.reply('You don\'t have a reminder with that ID.');
-        }
+        }*/
         return message.reply('Reminder '+args[0]+' has been deleted!');
     },
 };
