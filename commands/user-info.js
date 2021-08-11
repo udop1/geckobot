d82e723e8eebfb5afe06317072a796e101da2431
udop1 <52384714+udop1@users.noreply.github.com>
2021-08-11 12:50:26 +0100

    Delete user-info.js

--- commands/user-info.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'user-info',
-    description: 'Allows you to get info about a user.',
-    guildOnly: true,
-    execute(message, args) {
-        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-    },
-};
