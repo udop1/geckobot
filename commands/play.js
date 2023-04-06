e8e4778bbff98c62ff97cf7276d1ded6ed5311f0
udop1 <adamtaylor2001@live.co.uk>
2023-04-06 18:45:08 +0100

    Reverting ytdl-core to fix stream issues

--- commands/play.js
+++ commands/play.js
@@ -1,40 +1,42 @@
 module.exports = {
-	name: 'play',
-	description: 'Plays music',
+	name: "play",
+	description: "Plays music",
 	options: [
 		{
-			name: 'song',
-			type: '3', //STRING
-			description: 'Song you want to play',
-			required: true
+			name: "song",
+			type: "3", //STRING
+			description: "Song you want to play",
+			required: true,
 		},
 	],
 
 	async execute(interaction) {
 		await interaction.deferReply();
-		const { player, QueryType } = require('../index.js');
+		const { player, QueryType } = require("../index.js");
 		const playdl = require("play-dl");
-		const songQuery = interaction.options.getString('song');
+		const songQuery = interaction.options.getString("song");
 
 		try {
 			//Check if user is in VC
 			if (!interaction.member.voice.channel) {
-				return await interaction.editReply({ content: 'You\'re not in a voice channel', ephemeral: true });
+				return await interaction.editReply({ content: "You're not in a voice channel", ephemeral: true });
 			} else if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId != interaction.guild.members.me.voice.channelId) {
-				return await interaction.editReply({ content: 'You\'re not in my voice channel', ephemeral: true });
+				return await interaction.editReply({ content: "You're not in my voice channel", ephemeral: true });
 			}
 
 			//Search for user requested song
-			const searchResult = await player.search(songQuery, {
-				requestedBy: interaction.user,
-				searchEngine: QueryType.AUTO
-			}).catch((error) => {
-				console.log(error);
-			});
+			const searchResult = await player
+				.search(songQuery, {
+					requestedBy: interaction.user,
+					searchEngine: QueryType.AUTO,
+				})
+				.catch((error) => {
+					console.log(error);
+				});
 			if (!searchResult || !searchResult.tracks.length) {
 				return await interaction.editReply({ content: `No results found for ${songQuery}`, ephemeral: true });
 			} else if (searchResult.tracks[0].durationMS >= 3600000) {
-				return await interaction.editReply({ content: 'Song can\'t be longer than 1 hour', ephemeral: true });
+				return await interaction.editReply({ content: "Song can't be longer than 1 hour", ephemeral: true });
 			}
 
 			//Create queue
@@ -42,43 +44,43 @@ module.exports = {
 			if (!queue) {
 				var queue = player.nodes.create(interaction.guild, {
 					ytdlOptions: {
-						quality: 'highestaudio',
-						filter: 'audioonly',
+						quality: "highestaudio",
+						filter: "audioonly",
 					},
 					metadata: {
-						channel: interaction.channel
+						channel: interaction.channel,
 					},
 					disableVolume: true,
-	
+
 					async onBeforeCreateStream(track, source, _queue) {
 						if (source === "youtube") {
 							return (await playdl.stream(track.url, { discordPlayerCompatibility: true })).stream;
 						}
-					}
+					},
 				});
 			}
 
 			//Attempt VC connection
 			try {
-				if (!queue.connection) { //If not already connected
+				if (!queue.connection) {
+					//If not already connected
 					await queue.connect(interaction.member.voice.channel);
 				}
-
 			} catch {
 				queue.delete();
-				return await interaction.editReply({ content: 'Couldn\'t join your voice channel', ephemeral: true });
+				return await interaction.editReply({ content: "Couldn't join your voice channel", ephemeral: true });
 			}
 
 			//If there's a playlist, add all of them. Otherwise, just add the first result
 			searchResult.playlist ? queue.addTrack(searchResult.tracks) : queue.addTrack(searchResult.tracks[0]);
-			if (!queue.node.isPlaying()) { //If not already playing
+			if (!queue.node.isPlaying()) {
+				//If not already playing
 				await queue.node.play();
 			}
 			return await interaction.editReply({ content: `\:notes: Added **${searchResult.tracks[0].title}** (\`${searchResult.tracks[0].duration}\`) to the queue!` });
-
 		} catch (error) {
 			console.log(error);
 			return await interaction.editReply({ content: `${error}` });
 		}
-	}
-}
+	},
+};
--- commands/queue.js
+++ commands/queue.js
@@ -1,22 +1,22 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
-	name: 'queue',
-	description: 'Song queue',
+	name: "queue",
+	description: "Song queue",
 	options: [
 		{
-			name: 'view',
-			description: 'View the song queue',
+			name: "view",
+			description: "View the song queue",
 			type: 1, //SUBCOMMAND
 		},
 		{
-			name: 'delete',
-			description: 'Delete a song from the queue',
+			name: "delete",
+			description: "Delete a song from the queue",
 			type: 1, //SUBCOMMAND
 			options: [
 				{
-					name: 'position',
-					description: 'Queue position to delete',
+					name: "position",
+					description: "Queue position to delete",
 					type: 4, //INTEGER
 					required: true,
 				},
@@ -26,32 +26,31 @@ module.exports = {
 
 	async execute(interaction) {
 		await interaction.deferReply();
-		const { player } = require('../index.js');
-		const queuePos = interaction.options.getInteger('position') - 1; //Arr start 0
+		const { player } = require("../index.js");
+		const queuePos = interaction.options.getInteger("position") - 1; //Arr start 0
 
 		try {
 			//Check if user is in VC
 			if (!interaction.member.voice.channel) {
-				return await interaction.editReply({ content: 'You\'re not in a voice channel', ephemeral: true });
+				return await interaction.editReply({ content: "You're not in a voice channel", ephemeral: true });
 			} else if (interaction.guild.members.me.voice.channelId && interaction.member.voice.channelId != interaction.guild.members.me.voice.channelId) {
-				return await interaction.editReply({ content: 'You\'re not in my voice channel', ephemeral: true });
+				return await interaction.editReply({ content: "You're not in my voice channel", ephemeral: true });
 			}
 
 			const queue = player.nodes.get(interaction.guildId);
 
 			//Check if music is being played
 			if (!queue || !queue.node.isPlaying) {
-				return await interaction.editReply({ content: 'No music is currently being played' });
+				return await interaction.editReply({ content: "No music is currently being played" });
 			}
 
 			//Check if anything is in the queue
 			if (!queue.tracks.map((track, id) => ++id)) {
-				return await interaction.editReply({ content: 'Nothing currently in the queue' });
+				return await interaction.editReply({ content: "Nothing currently in the queue" });
 			}
 
-
 			//View command
-			if (interaction.options.getSubcommand() === 'view') {
+			if (interaction.options.getSubcommand() === "view") {
 				//Seperates the queue into specific blocks
 				var embedLength = 25;
 				var queueArrTitle = queue.tracks.map((track) => track.title);
@@ -59,25 +58,23 @@ module.exports = {
 				var queueArrRequested = queue.tracks.map((track) => track.requestedBy.username);
 				var queueArrURL = queue.tracks.map((track) => track.url);
 
-				var splitArrTitle = new Array(Math.ceil(queueArrTitle.length / embedLength)).fill().map(_ => queueArrTitle.splice(0, embedLength));
-				var splitArrAuthor = new Array(Math.ceil(queueArrAuthor.length / embedLength)).fill().map(_ => queueArrAuthor.splice(0, embedLength));
-				var splitArrRequested = new Array(Math.ceil(queueArrRequested.length / embedLength)).fill().map(_ => queueArrRequested.splice(0, embedLength));
-				var splitArrURL = new Array(Math.ceil(queueArrURL.length / embedLength)).fill().map(_ => queueArrURL.splice(0, embedLength));
+				var splitArrTitle = new Array(Math.ceil(queueArrTitle.length / embedLength)).fill().map((_) => queueArrTitle.splice(0, embedLength));
+				var splitArrAuthor = new Array(Math.ceil(queueArrAuthor.length / embedLength)).fill().map((_) => queueArrAuthor.splice(0, embedLength));
+				var splitArrRequested = new Array(Math.ceil(queueArrRequested.length / embedLength)).fill().map((_) => queueArrRequested.splice(0, embedLength));
+				var splitArrURL = new Array(Math.ceil(queueArrURL.length / embedLength)).fill().map((_) => queueArrURL.splice(0, embedLength));
 
 				//Creates embed and fills with split information
 				var queueEmbed = [];
 				var firstEmbed = true;
 				for (var i = 0; i < splitArrTitle.length; i++) {
 					queueEmbed[i] = new EmbedBuilder();
-					queueEmbed[i].setColor('#0099ff');
-					queueEmbed[i].setTitle('Queue:');
+					queueEmbed[i].setColor("#0099ff");
+					queueEmbed[i].setTitle("Queue:");
 					queueEmbed[i].setFooter({ text: `Page ${i + 1}` });
 					queueEmbed[i].setTimestamp();
 
 					for (var j = 0; j < splitArrTitle[i].length; j++) {
-						queueEmbed[i].addFields(
-							{ name: `Position: ${(embedLength * i) + j + 1}`, value: `Title: ${splitArrTitle[i][j]}\nAuthor: ${splitArrAuthor[i][j]}\nRequested By: ${splitArrRequested[i][j]}\n\u200b\nURL: ${splitArrURL[i][j]}`, inline: true },
-						);
+						queueEmbed[i].addFields({ name: `Position: ${embedLength * i + j + 1}`, value: `Title: ${splitArrTitle[i][j]}\nAuthor: ${splitArrAuthor[i][j]}\nRequested By: ${splitArrRequested[i][j]}\n\u200b\nURL: ${splitArrURL[i][j]}`, inline: true });
 					}
 
 					if (firstEmbed == true) {
@@ -90,21 +87,20 @@ module.exports = {
 			}
 
 			//Delete command
-			else if (interaction.options.getSubcommand() === 'delete') {
+			else if (interaction.options.getSubcommand() === "delete") {
 				if (queuePos > queue.tracks.size && queuePos < queue.tracks.size) {
-					return await interaction.editReply({ content: 'There is nothing in this position in the queue' });
+					return await interaction.editReply({ content: "There is nothing in this position in the queue" });
 				}
 
 				var removedSong = queue.tracks[queuePos].title;
 
-				queue.remove(queuePos);
+				queue.node.remove(queuePos);
 
 				return await interaction.editReply({ content: `Removed **${removedSong}** from the queue` });
 			}
-
 		} catch (error) {
 			console.log(error);
 			return await interaction.editReply({ content: `${error}` });
 		}
-	}
-}
+	},
+};
