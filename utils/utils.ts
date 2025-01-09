55355b276b39898478751b095f956791c6a32b79
udop1 <adamtaylor2001@live.co.uk>
2025-01-09 23:22:42 +0000

    Transferring utils to TS

---
+++ utils/utils.ts
@@ -0,0 +1,27 @@
+import { Queue } from 'distube';
+
+export const numberWithCommas = (number: number) => {
+	return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+};
+
+// export const queueStatus = (queue: Queue) => {
+// 	return `**Volume:** \`${queue.volume}%\` | **Filters:** \`${
+// 		queue.filters.names.join(', ') || 'OFF'
+// 	}\` | **Loop:** \`${
+// 		queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'OFF'
+// 	}\` | **Autoplay:** \`${queue.autoplay ? 'ON' : 'OFF'}\``;
+// };
+
+export const queueStatus = (queue: Queue) => {
+	let repeatModeStatus: 'All Queue' | 'This Song' | 'OFF';
+
+	if (queue.repeatMode === 2) {
+		repeatModeStatus = 'All Queue';
+	} else if (queue.repeatMode === 1) {
+		repeatModeStatus = 'This Song';
+	} else {
+		repeatModeStatus = 'OFF';
+	}
+
+	return `**Volume:** \`${queue.volume}%\` | **Filters:** \`${queue.filters.names.join(', ') || 'OFF'}\` | **Loop:** \`${repeatModeStatus}\` | **Autoplay:** \`${queue.autoplay ? 'ON' : 'OFF'}\``;
+};
